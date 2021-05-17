c718bbc3d6c3b3a9f294feb78eada4287e80e210
Jennychangxuezhen <1094530839@qq.com>
2021-05-17 16:52:07 +0800

    表单表格翻页器组件及demo添加

--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -2,5 +2,9 @@ export default [
   {
     path: '/detail',
     component: () => import('@/views/login/LoginHome')
+  },
+  {
+    path: '/useDemo',
+    component: () => import('@/views/demo/useDemo')
   }
 ];
